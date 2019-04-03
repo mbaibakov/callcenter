95094bfc7c05e00a383cbb62bd81a1c9a94f8406
Mikhail Baibakov <mihishaha@mail.ru>
2019-04-03 16:21:53 +0300

    3 listeners

--- api/src/main/kotlin/center/orbita/callcenter/servicestatus/ServiceStatusRabbitListener.kt
+++ api/src/main/kotlin/center/orbita/callcenter/servicestatus/ServiceStatusRabbitListener.kt
@@ -29,15 +29,15 @@ class ServiceStatusRabbitListener(private val repository: ServiceStatusRepositor
             val msg = mapper.readValue(body, ServiceStatusMessage::class.java)
             logger.debug("Incoming object $msg")
 
-//            val existEntity = repository.getByNumber(msg.number)
-//            val newEntity = existEntity?.copy(statusCode = msg.statusCode,
-//                    statusDescription = msg.statusDescription,
-//                    creationDate = msg.creationDate,
-//                    releaseDate = msg.releaseDate)
-//                    ?: msg.convertToEntity()
-//            logger.debug("New entity $newEntity")
+            val existEntity = repository.getByNumber(msg.number)
+            val newEntity = existEntity?.copy(statusCode = msg.statusCode,
+                    statusDescription = msg.statusDescription,
+                    creationDate = msg.creationDate,
+                    releaseDate = msg.releaseDate)
+                    ?: msg.convertToEntity()
+            logger.debug("New entity $newEntity")
 
-//            repository.save(newEntity)
+            repository.save(newEntity)
         } catch (e: IOException) {
             logger.error(e.localizedMessage, e)
         }
